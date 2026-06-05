38ca2329841c0e9473bc061f9caac492878b4780
Ishtiaqahmad06 <iaishtiaqahmad06@gmail.com>
2026-06-05 09:15:13 +0500

    intial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import ThemeProvider from "./Context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <ThemeProvider>
